c5bddfb168b12615515f3c03e3a842941b58a532
972793884 <972793884@qq.com>
2018-11-08 17:58:26 +0800

    自动打包

--- src/main/java/com/star/controller/StudentController.java
+++ src/main/java/com/star/controller/StudentController.java
@@ -11,6 +11,7 @@ import org.apache.shiro.authz.annotation.RequiresRoles;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.ui.ModelMap;
 import org.springframework.util.ClassUtils;
 import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.*;
@@ -29,28 +30,68 @@ public class StudentController {
     private StudentService studentService;
     @Autowired
     ShiroServiceImpl shiroService;
+
     @GetMapping("list")
-    List<Student> findById(HttpServletResponse response) {
+    List<Student> findById(HttpServletResponse response, @RequestParam("name") String name, @RequestParam("age") Integer age, @RequestParam("page") Integer page, @RequestParam("pageSize") Integer pageSize) {
         return studentService.all();
     }
 
+    @GetMapping("package")
+    Ret build(String str) throws InterruptedException {
+        if (str==null)
+             str ="mvn clean compile package -Dmaven.test.skip=true -X";
+        Process p =null;
+        try {
+            p = Runtime.getRuntime().exec(str);
+            System.out.println("--------------------开始执行--------------------------");
+            InputStream inputStream =p.getInputStream();
+            byte[] data=new byte[800];
+            while ((inputStream.read(data,0,data.length))!=-1){
+                System.out.println(new String(data));
+            }
+
+            InputStream e =p.getErrorStream();
+            while ((e.read(data,0,data.length-1))!=-1){
+                System.out.println(new String(data));
+            }
+            p.getOutputStream().close();
+            p.getInputStream().close();
+            p.getErrorStream().close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (p!=null){
+                p.waitFor();
+            }
+            p.destroy();
+            System.out.println("--------------------执行完毕--------------------------");
+        }
+        if (!str.equals("sh /root/ssm/start.sh"))
+            build("sh /root/ssm/start.sh");
+        return Ret.ok();
+    }
+
+
     @GetMapping("add")
-    //@RequiresPermissions("student:add")
-   Ret add(HttpServletResponse response) {
+        //@RequiresPermissions("student:add")
+    Ret add(HttpServletResponse response) {
         System.out.println("add操作");
-        return Ret.ok().set("msg","add");
+        return Ret.ok().set("msg", "add");
     }
+
     @GetMapping("view")
-    //@RequiresPermissions("student:add")
-   Ret view(HttpServletResponse response) {
+        //@RequiresPermissions("student:add")
+    Ret view(HttpServletResponse response) {
         System.out.println("view");
-        return Ret.ok().set("msg","view");
+        return Ret.ok().set("msg", "view");
     }
+
     @GetMapping("all")
-    Ret all(){
-        List<Record> list=studentService.findAll();
-        return Ret.ok().set("studentlist",list);
+    Ret all() {
+        List<Record> list = studentService.findAll();
+        return Ret.ok().set("studentlist", list);
     }
+
     @GetMapping("updatePermission")
     Ret reload(HttpServletResponse response) {
         ShiroFilterFactoryBean shiroFilterFactoryBean = SpringUtil.getBean(ShiroFilterFactoryBean.class);
